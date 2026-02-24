770ecd329822695f37a6552a7fcf87c563e18409
farhan-al-shahrier-siam <farhanalshahriersiam@gmail.com>
2026-02-24 06:30:50 +0600

    interview button is live with proper functionality

--- script/script.js
+++ script/script.js
@@ -5,10 +5,13 @@ let rejectedCount = document.getElementById("rejectedCount");
 let interviewList = [];
 let rejectedList = [];
 
+const allcards = document.getElementById('all-job-cards');
+const interviewFilteredSection = document.getElementById("interview-filtered-section");
+
 function countCalculator() {
     // counting total job cards and added the number to the total in head
-    const allcards = document.getElementById("all-job-cards").children.length;
-    total.innerText = allcards;
+    
+    total.innerText = allcards.children.length;
     interviewCount.innerText = interviewList.length;
     rejectedCount.innerText = rejectedList.length;
 }
@@ -30,48 +33,58 @@ function toggleButtons(id) {
     const selectedBtn = document.getElementById(id);
     selectedBtn.classList.remove("bg-white");
     selectedBtn.classList.add("bg-primary", "text-white");
+
+    if(id =='interview-filter-btn'){
+        allcards.classList.add('hidden');
+        interviewFilteredSection.classList.remove('hidden')
+    } else if(id == 'all-filter-btn'){
+        allcards.classList.remove('hidden');
+        interviewFilteredSection.classList.add('hidden')
+    }
 }
 
 const mainContainer = document.querySelector("main");
 
 mainContainer.addEventListener("click", function (event) {
-    const parentNode = event.target.parentNode.parentNode;
-    const companyName = parentNode.querySelector(".companyName").innerText;
-    const jobTitle = parentNode.querySelector(".jobTitle").innerText;
-    const jobtype = parentNode.querySelector(".jobtype").innerText;
-    const jobStatus = parentNode.querySelector(".jobStatus").innerText;
-    const jobDescription = parentNode.querySelector(".jobDescription").innerText;
+    if (event.target.classList.contains("interview-btn")) {
+        const parentNode = event.target.parentNode.parentNode;
+        const companyName = parentNode.querySelector(".companyName").innerText;
+        const jobTitle = parentNode.querySelector(".jobTitle").innerText;
+        const jobtype = parentNode.querySelector(".jobtype").innerText;
+        const jobStatus = parentNode.querySelector(".jobStatus").innerText;
+        const jobDescription = parentNode.querySelector(".jobDescription").innerText;
 
-    const cardInfo = {
-        companyName,
-        jobTitle,
-        jobtype,
-        jobStatus,
-        jobDescription
-    };
+        const cardInfo = {
+            companyName,
+            jobTitle,
+            jobtype,
+            jobStatus,
+            jobDescription,
+        };
 
-    const companyExist = interviewList.find(item => item.companyName == cardInfo.companyName)
-    if(!companyExist){
-        interviewList.push(cardInfo);
+        const companyExist = interviewList.find((item) => item.companyName == cardInfo.companyName);
+        parentNode.querySelector(".jobStatus").innerHTML= `<p class="jobStatus "><span class="text-green-500 border rounded-sm py-2 px-3">INTERVIEW</span></p>`;
+        if (!companyExist) {
+            interviewList.push(cardInfo);
+        }
+        countCalculator();
+        renderInterviewSection();
     }
-    countCalculator();
-    renderInterviewSection();
     
 });
 
-function renderInterviewSection(){
-    const filteredSection = document.getElementById('filtered-section');
-    filteredSection.innerHTML = '';
-    for(item of interviewList){
-        let div = document.createElement('div');
-        div.className = 'job-card p-6 flex justify-between bg-white rounded-xl shadow'
+function renderInterviewSection() {
+    interviewFilteredSection.innerHTML = "";
+    for (item of interviewList) {
+        let div = document.createElement("div");
+        div.className = "job-card p-6 flex justify-between bg-white rounded-xl shadow";
         div.innerHTML = `
         <div>
-            <h2 class="companyName font-semibold text-xl mb-1">Mobile First Corp</h2>
-            <p class="jobTitle text-[#64748B]">React Native Developer</p>
-            <p class="jobtype text-gray-400 my-5">Remote • Full-time • $130,000 - $175,000</p>
-            <p class="jobStatus "><span class="bg-gray-200 rounded-sm py-2 px-3">Not Applied</span></p>
-            <p class="jobDescription text-gray-600 mt-3">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+            <h2 class="companyName font-semibold text-xl mb-1">${item.companyName}</h2>
+            <p class="jobTitle text-[#64748B]">${item.jobTitle}</p>
+            <p class="jobtype text-gray-400 my-5">${item.jobtype}</p>
+            <p class="jobStatus "><span class="text-green-500 border rounded-sm py-2 px-3">INTERVIEW</span></p>
+            <p class="jobDescription text-gray-600 mt-3">${item.jobDescription}</p>
             <div class="mt-5 flex gap-3">
                 <button class="cursor-pointer interview-btn text-green-500 py-2 px-3 border rounded-sm">INTERVIEW</button>
                 <button class="cursor-pointer rejected-btn text-red-500 py-2 px-3 border rounded-sm">REJECTED</button>
@@ -81,7 +94,7 @@ function renderInterviewSection(){
         <div>
             <button class="btn-delete cursor-pointer p-1 border rounded-sm"><i class="fa-solid fa-trash-can"></i></button>
         </div>
-        `
-        filteredSection.appendChild(div);
+        `;
+        interviewFilteredSection.appendChild(div);
     }
 }
